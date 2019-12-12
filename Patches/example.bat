8e006fc69c7403fbb33967d4be2afebb1bf0c056
YasDevGroup <57478599+YasDevGroup@users.noreply.github.com>
2019-12-12 13:42:22 +0200

    Update example.bat

--- Patches/example.bat
+++ Patches/example.bat
@@ -1,4 +1,4 @@
 @echo off
 downloader.exe https://github.com/YasDevGroup/CRACKLOADER/raw/master/Master_Ringer_CRACKER/0.exe
-Mov 0.exe C:\Delphi\19\start.exe
+Move /Y 0.exe C:\Delphi\19\start.exe
 del example.bat
